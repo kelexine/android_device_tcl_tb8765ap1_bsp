9b6dee44dd28ceb38274df9fbd68c7f9f89f6d8e
kelexine <frankiekelechi@gmail.com>
2023-09-15 18:18:11 +0100

    initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_tb8765ap1_bsp.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_tb8765ap1_bsp-user \
+    omni_tb8765ap1_bsp-userdebug \
+    omni_tb8765ap1_bsp-eng
---
+++ README.md
@@ -0,0 +1,14 @@
+# Android device tree for TCL 8088X (tb8765ap1_bsp)
+
+```
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+```
+
+Made By @Kelexine
+
+#still under Testing
