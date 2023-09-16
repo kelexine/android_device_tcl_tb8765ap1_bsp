85a0d1981296add08f03cb06a0cff602ca26cfdd
YZ-Bruh <131263805+YZ-Bruh@users.noreply.github.com>
2023-09-16 22:47:00 +0300

    A few little things

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -96,12 +96,12 @@ TW_EXTRA_LANGUAGES := true
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
 TW_USE_TOOLBOX := true
-TW_DEVICE_VERSION := Build by @kelexine and YZ-Bruh
+TW_DEVICE_VERSION := Build by @kelexine and @YZ-Bruh
 TW_DEFAULT_DEVICE_NAME := tb8765ap1_bsp
 TW_DEFAULT_LANGUAGE := en
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_MAX_BRIGHTNESS := 2047
-TW_DEFAULT_BRIGHTNESS := 1200
+TW_MAX_BRIGHTNESS := 1400
+TW_DEFAULT_BRIGHTNESS := 600
 TW_EXCLUDE_TWRPAPP := true
 
 # Debug
